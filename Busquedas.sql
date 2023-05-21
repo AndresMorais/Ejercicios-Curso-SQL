e5ab5134260fc97c7637f0e5a1c4c6d49e50f453
AndresMorais <andres.decmorais@gmail.com>
2023-05-21 01:07:01 -0300

    Búsquedas Avanzdas
    Ejercicos de búsquedas con las funciones CONTAINS,FORMSOF Y FREETEXT

--- Busquedas.sql
+++ Busquedas.sql
@@ -76,3 +76,69 @@ SECCION QUINTA DEL CURSO: FULL TEXT SEARCH
 ------------------------------------------
 -----------------------------------------*/
 
+--CONTAINS 
+
+-- cuando la búsqueda es sobre una frase, se usa comillas dobre entre comillas simples
+select * from [Person].[Address] A 
+where contains (A.AddressLine1,'"First Avenue"') 
+
+--CONTAINS /AND/AND NOT/OR
+
+select * from [Person].[Address] A
+where contains (A.AddressLine1, 'Avenue AND Port')
+
+select * from [Person].[Address] A
+where contains (A.AddressLine1,'Avenue AND Port AND NOT 54')
+
+select * from [Person].[Address] A
+where contains (A.AddressLine1,'Avenue OR Port')
+
+
+-- el parentesis dentro de las comillas permite la ejecución del operador AND 
+select * from [Person].[Address] A
+where contains (A.AddressLine1,'(36 AND Avenue) OR Port') 
+
+--CONTAINS NEAR
+
+select * from Person.Address
+where contains (AddressLine1,'"5415 San" NEAR Dr.')
+
+
+--CONTAINS NEAR (búsqueda de mas de una palabra)
+
+select * from Person.Address
+where contains (AddressLine1,'NEAR((21,centrale),6)')-- trae todos los registros que tengan hasta 6 palabras entre '21' y 'Centrale'
+
+select * from Person.Address --order by len( AddressLine1) desc
+where contains (AddressLine1,'NEAR((Hilton, Factory,25),5)') --busca hasta 5 palabras entre 'Hilton' y 'Factory' y luego hasta 5 palabras entre 'Factory' y '25'
+
+
+
+--FORMSOF INFLECTIONAL
+--si el valor uscado es sustativo, considera el singilar, plural y genero. Si es verbo, trae toda las formas verbales.
+
+select * from Person.Address
+where contains (AddressLine1,'FORMSOF(INFLECTIONAL,"Streets")')
+
+--ejemplo con verbo
+--update Person.Address set AddressLine1='6387 Scenic Avenue hacemos' where AddressID=8
+
+select * from  Person.Address
+where contains (AddressLine1,'FORMSOF(INFLECTIONAL,"hagamos")', LANGUAGE 3082) --3082 corresponde al  LocalID del español 
+
+
+--OBTENER TODOS LOS TIEMPOS VERBALES DE UN VERBO
+
+select display_term 
+from SYS.dm_fts_parser('FORMSOF(INFLECTIONAL,"TRABAJO")',3082,0,0)
+
+--FREETEXT. trae todos los registros que contiene alguna de las palabras del predicado 
+
+select * from Person.Address
+where freetext (AddressLine1,'"street Avenue"') 
+
+
+--POPULATION DE UNA TABLA. Actualiza los catalogos para la aplicacion de fulltext search
+
+ALTER FULLTEXT INDEX ON Person.Address START FULL POPULATION
+ALTER FULLTEXT INDEX ON Person.Address START UPDATE POPULATION --Actualización incremental 
